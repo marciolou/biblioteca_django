e0fa64c819be629b4bc0ceea52b2986fe8e4f37f
marciolou <marcioalbertoo.lourenco@gmail.com>
2023-01-25 13:31:55 -0300

    Ultimos ajutes para validacao

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -9,6 +9,3 @@ urlpatterns = [
     path('', include('usuarios.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
